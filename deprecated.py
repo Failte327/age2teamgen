47f50b07a50089b28c5d38eff5ced047be8eb296
Timothy Twelker <timothy.twelker@gmail.com>
2024-05-02 10:02:56 -0600

    Migrated script functionality into an endpoint, returns ratings on the siten ow

--- deprecated.py
+++ deprecated.py
@@ -1,3 +1,5 @@
+# THIS FILE IS DEPRECATED, FUNCTIONALITY HAS BEEN MOVED INTO INTERFACE.PY
+
 import requests
 from bs4 import BeautifulSoup
 
--- interface/interface.py
+++ interface/interface.py
@@ -1,9 +1,45 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
-def age_of_empires_team_generator():
+def render_homepage():
     return render_template("base.html")
+
+@app.route('/generate_teams')
+def team_generator():
+    user_id = 4289859 # Sean's user id, he's in most games so he's the easiest one to query
+    url = f"https://www.aoe2insights.com/user/{user_id}/matches/?ladder=0&player=&map=&played_civilization=&opponent_civilization=&duration=&position="
+
+    response = requests.get(url)
+    raw_data = response.text
+
+    parsed_data = BeautifulSoup(raw_data, features="lxml")
+
+    players = []
+    player_ratings = {}
+
+    links = parsed_data.select(".team-player")
+    for link in links:
+        if link.find("a") is not None:
+            link_string = link.find("a").string.strip()
+            if link_string not in players:
+                players.append(link_string)
+            
+
+    for player in players:
+        for link in links:
+            if link.find("a") is not None:
+                link_string = link.find("a").string.strip()
+                if link_string == player:
+                    if len(link.parent.select(".rating > span")) > 0:
+                        rating = link.parent.select(".rating > span")[0].string
+                        if player not in player_ratings:
+                            player_ratings[player] = rating
+
+    print(player_ratings)
+    return player_ratings
 
 ## run cmd: flask --app interface run
