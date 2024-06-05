568856b7365126191a2668b847e3c08451806478
TC Twelker <timothy.twelker@securitymetrics.com>
2024-06-05 16:25:08 -0600

    Refined the team creation algorithm greatly to account for inaccurate elos in our specific group, it's calculating fairly even teams at the moment

--- interface/interface.py
+++ interface/interface.py
@@ -12,21 +12,26 @@ def render_homepage():
 
 @app.route('/generate_teams',  methods=['POST'])
 def team_generator():
+    # Strip whitespace
     player_list = request.form.get("playername").split(",")
-    print(player_list)
+    counter = 0
+    for i in player_list:
+        new = i.strip()
+        player_list[counter] = new
+        counter = counter + 1
 
+    # Get stats from aoe2insights.com
     user_id = 4289859 # Sean's user id, he's in most games so he's the easiest one to query
     url = f"https://www.aoe2insights.com/user/{user_id}/matches/?ladder=0&player=&map=&played_civilization=&opponent_civilization=&duration=&position="
-
     response = requests.get(url)
     raw_data = response.text
-
     parsed_data = BeautifulSoup(raw_data, features="lxml")
 
+    # Construct data objects linking players to their ratings
+    
     players = []
     active_players = {}
     player_ratings = {}
-
     links = parsed_data.select(".team-player")
     for link in links:
         if link.find("a") is not None:
@@ -44,14 +49,41 @@ def team_generator():
                         if player not in player_ratings:
                             player_ratings[player] = rating
 
+    alternate_lookup_ids = [7436245, 2804382, 7451904, 10061690]
     for i in player_list:
-        print(i)
-        for key, value in player_ratings.items():
-            if i == key:
-                active_players[key] = value
+        if i == "meghalb":
+                active_players[i] = "800"
+        elif i == "brandonnelson68":
+            active_players[i] = "800"
+        elif i == "Teancum00":
+            active_players[i] = "800"
+        else:
+            for key, value in player_ratings.items():
+                if i == key:
+                    active_players[key] = value
+            # Get stats from other profile pages if we can't find the player
+            if i not in player_ratings.keys():
+                no_rating = True
+                while no_rating is True:
+                    for n in alternate_lookup_ids:
+                        user_id = n
+                        url = f"https://www.aoe2insights.com/user/{user_id}/matches/?ladder=0&player=&map=&played_civilization=&opponent_civilization=&duration=&position="
+                        response = requests.get(url)
+                        raw_data = response.text
+                        parsed_data = BeautifulSoup(raw_data, features="lxml")
+                        links = parsed_data.select(".team-player")
+                        for link in links:
+                            if link.find("a") is not None:
+                                link_string = link.find("a").string.strip()
+                                if link_string == i:
+                                    players.append(link_string)
+                                    if len(link.parent.select(".rating > span")) > 0:
+                                        rating = link.parent.select(".rating > span")[0].string
+                                        active_players[i] = rating
+                                        no_rating = False
 
+    # Create random player combinations until we have a difference in rating below the acceptable threshold
     team_count = (len(active_players)) / 2
-
     teams_set = False
 
     while teams_set is False:
